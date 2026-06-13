d0dca85c2404ec5e82fa84c2f1a4db401aa27838
navjot-ai-dev <navjotsingh542419@gmail.com>
2026-06-13 20:59:36 +0530

    first commit

---
+++ react-css/src/components/section1/section1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const section1 = () => {
+  return (
+       <div className ='h-screen w-full bg-amber-600'>
+    <h1>navjot</h1>
+    </div>
+  )
+}
+
+export default section1
